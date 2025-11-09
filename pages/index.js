86a74e3bd3e68d8ba10188696f7484563441045a
AugustoAU <94239501+AugustoAU@users.noreply.github.com>
2025-11-09 03:22:04 +0000

    modificar frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Daniele, eu amo você e se você tambem me ama, manda uma foto sorrindo</h1>
+    return <h1>Daniele, eu amo você e se você tambem me ama, manda uma foto sorrindo pra mim</h1>
 }
 
 export default Home;
